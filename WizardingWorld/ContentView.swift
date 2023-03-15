3b6e1a0f5529684f5143896455d52d005ea4d557
Biagio <biagior00@gmail.com>
2023-03-15 14:18:54 +0100

    Initial Commit

---
+++ WizardingWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WizardingWorld
+//
+//  Created by Biagio Ricci on 15/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WizardingWorld/WizardingWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  WizardingWorldApp.swift
+//  WizardingWorld
+//
+//  Created by Biagio Ricci on 15/03/23.
+//
+
+import SwiftUI
+
+@main
+struct WizardingWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
